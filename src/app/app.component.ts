50d03118e3a2f1c41309725a78957a1856fb263b
Anilitka <berishviliannaa@gmail.com>
2023-03-20 14:16:32 +0400

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare let window:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular122';
+  formModal:any;
+
+  ngOnInit(): void {
+  this.formModal = new window.bootstrap.Modal(
+  document.getElementById("exampleModal")
+);
+  }
+  openModal(){
+    this.formModal.show();
+  }
+  closeModal(){
+    this.formModal.hide();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from '../assets/navbar/navbar.component';
+import { FooterComponent } from '../assets/footer/footer.component';
+import { ModalComponent } from './modal/modal.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
